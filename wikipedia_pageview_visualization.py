6b872342e6b074657f351fc605c89643d6e0fee2
Greyson Brothers <greysonbrothers@gmail.com>
2020-01-30 01:48:32 -0800

    Update wikipedia_pageview_visualization.py

--- wikipedia_pageview_visualization.py
+++ wikipedia_pageview_visualization.py
@@ -4,50 +4,61 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 plt.rcParams['animation.ffmpeg_path'] = '/usr/local/bin/ffmpeg'
+plt.rcParams['axes.labelweight'] = 'bold'
+plt.rcParams['font.weight'] = 'bold'
 import matplotlib.animation as animation
 
-# Visualizing wikipedia top pageview data from a previous project with an animated bar chart
+
+# Visualizing wikipedia pageview data from a previous project with an animated bar chart
 
 """ DATA """
 
 data = pd.read_csv("alldata.csv")
 
-number_of_frames = data.shape[0]
-frame_rate = 1
+number_of_frames = data.shape[0]*2
+frame_rate = 50  # how fast do you want info to fly at you? 25
 fig,ax = plt.subplots(figsize=(15,8))
+x_range = int(4e+6)
+
+Names = [data.iloc[i][3:13] for i in data.index]
+Values = [data.iloc[i][13:] for i in data.index]
 
 
 """ BAR CHART DRAWING FUNCTION """
 
-def draw(frames, data):
+def draw(frame, data):
     ax.clear()
-    ax.barh(np.arange(len(data.iloc[frames][13:])), data.iloc[frames][13:], ec='k', color='#82baef',
+    ax.barh(np.arange(len(Values[frame])), Values[frame], ec='k', color='#82baef',
             align='center', alpha = 0.6, linewidth = 2)
     
-    """ LABELS """
-    clr = '#777777'
+    """ AXIS LABELS """
+    clr = '#363636'
     ax.invert_yaxis()
-    ax.set_yticks(np.arange(len(data.iloc[frames][3:13])))
-    ax.set_yticklabels(data.iloc[frames][3:13], color=clr, size=12)
-    ax.set_xlabel('\nView Count per Day', color=clr, size=12)
-    ax.tick_params(axis='x', colors=clr, labelsize=12)              
-    ax.set_title('HIGHEST VIEWED WIKIPEDIA PAGES\n', color=clr, size=12)
-    ax.text(0.005,1.05, f'DATE: {data.Month[frames]}.{data.Day[frames]}.{data.Year[frames]}', transform=ax.transAxes, 
-            ha='left', color='w', bbox=dict(facecolor='#82baef', alpha=0.8, edgecolor='w'))
+    ax.set_yticks(np.arange(0))
+    ax.set_xlabel('\n\nView Count per Day', color=clr, size=12)
+    ax.tick_params(axis='x', colors=clr, labelsize=10)              
+    ax.set_title('HIGHEST VIEWED WIKIPEDIA PAGES\n', color=clr, size=12, weight='bold')
+    ax.text(0.009,1.05, f'{data.Month[frame]}.{data.Day[frame]}.{data.Year[frame]}', transform=ax.transAxes, 
+            ha='left', va='bottom', color=clr, bbox=dict(fc='w', alpha=0.6, ec='k', linewidth='2', pad=6))
+    
+    """ BAR LABELS """  
+    for i, (value, name) in enumerate(zip(Values[frame], Names[frame])):
+        ax.text(value, i, '  ' + name, size=10, weight=600, ha='left', va='bottom')
+        ax.text(value, i+0.3, '   ' + str(value), size=8, color='#444444', ha='left', va='bottom')
     
     """ AXIS FORMAT """
-    plt.xticks(rotation=50)
+    plt.xticks(rotation=30)  # angle of x-axis labels
     plt.box(False)
-    ax.set_xlim(0,2000000)
+    ax.set_xlim(0, x_range)
     ax.margins(0, 0.01)
-    ax.grid(which='major', axis='x', linestyle='-')
+    ax.grid(which='major', axis='x', linestyle='-', color='#DCDCDC')
     ax.set_axisbelow(True)
-    plt.gcf().subplots_adjust(left=0.3, bottom=0.2)
+    plt.gcf().subplots_adjust(left=0.02, bottom=0.2)
 
+# Produce the Animatoin
 anim = animation.FuncAnimation(fig, draw, number_of_frames, fargs=(data, ), interval=(1/frame_rate)*1000, blit=False)
 plt.show()  
 
-#Set up formatting for the movie files
-Writer = animation.writers['pillow']
-writer = Writer(fps=frame_rate, metadata=dict(artist='Me'), bitrate=1800)
-anim.save('mymovie.mp4',writer=writer)
+# Format & save the animation file
+FFwriter=animation.FFMpegWriter(fps=10, extra_args=['-vcodec', 'libx264'])
+anim.save('plswork.mp4', writer=FFwriter, dpi=100)

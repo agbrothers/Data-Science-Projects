29cc69f4c8ac4ac2c7c79d88b1476bcb5efde2fa
Greyson Brothers <greysonbrothers@gmail.com>
2020-02-23 15:43:40 -0800

    Update audio_feature_extractor.py

--- audio_feature_extractor.py
+++ audio_feature_extractor.py
@@ -17,9 +17,20 @@ def minMaxNormalize(arr):
     mx = np.max(arr)
     return((arr-mn) / (mx-mn))
 
-def processAudio(audio, label):
-    noise = audio[0:int(len(audio)/8)]
-    audio = nr.reduce_noise(audio, noise, verbose=False)
+def findNoise(audio):
+    # Split the audio clip into 8 even intervals
+    step_size = int(audio.shape[0]/8)
+    intervals = [[i, i + step_size] for i in range(0, audio.shape[0] - step_size, step_size)]
+    
+    # Find the variance of the change in amplitude over each interval and take the min as the noisey interval
+    std = [np.std(audio[i[0]:i[1]]) for i in intervals]
+    noisy_part = intervals[np.argmin(std)]
+    noise = audio[noisy_part[0]:noisy_part[1]]
+    return(noise)
+
+def processAudio(file, label):
+    audio, rate = librosa.load(directory + '/' + folder + '/' + file)
+    audio = nr.reduce_noise(audio, findNoise(audio), verbose=False)
     audio, index = librosa.effects.trim(audio, top_db=20, frame_length=512, hop_length=64)
         
     stft = np.abs(librosa.stft(audio, n_fft=512, hop_length=256, win_length=512))
@@ -42,7 +53,6 @@ for folder in os.listdir(directory):
     for file in os.listdir(directory + '/' + folder + '/'):
         if file == '.DS_Store':
             continue    
-        audio, rate = librosa.load(directory + '/' + folder + '/' + file)
-        master_df = master_df.append( processAudio(audio, folder))
+        master_df = master_df.append( processAudio(file, folder))
         
 master_df.to_csv('preprocessed_audio.csv')
